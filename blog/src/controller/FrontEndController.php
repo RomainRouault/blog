b11e51971580b9d3365d54c623165511581eb7e6
CharlesdeGore <rrouault11@gmail.com>
2018-03-27 22:55:52 +0200

    Include Twig in the controller

--- blog/src/controller/FrontEndController.php
+++ blog/src/controller/FrontEndController.php
@@ -7,16 +7,19 @@ use Blog\Model\PostManager;
 class FrontEndController()
 {
 
+	public function __construct() 
+	{
+		$loader = new \Twig_Loader_Filesystem('./view/front');
+		$twig = new \Twig_Environment($this->$loader);
+	}
+
 	function blog()
 	{
-		$postManager = new PostManager();
-		$blogPosts = $postManager->getPublishedPosts();
-
 		if (empty($blogPosts)){
 			throw new Exception('Il n\'y a pas encore d\'article publié');
 		}
 
-		require('./view/front/blogView.html.twig');
+		return $twig->render('blogView.html.twig'), array('blogPost' => $blogPosts);
 	}
 
 }
