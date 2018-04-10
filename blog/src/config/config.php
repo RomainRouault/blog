5a79dfc7effcfc9c9bddce580c356cc1ff294d6d
CharlesdeGore <rrouault11@gmail.com>
2018-04-10 23:59:10 +0200

    Create config file
    +newbackendlayout twig

---
+++ blog/src/config/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+*Class Config with constants to sets database
+*
+*/
+
+namespace Blog\Config;
+
+Abstract Class Config
+{
+    const DB_HOST    = 'localhost',
+    	  DB_NAME      = 'blog',
+          DB_USERNAME  = 'root',
+          DB_PASSWORD  = '';
+}
+
