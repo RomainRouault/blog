abc03126e8922d576df6f20b12644cf2ecb2736b
CharlesdeGore <rrouault11@gmail.com>
2018-04-10 23:45:03 +0200

    Create Router - Back End Integration
    +create config file
    +Advanced on Post Issue

--- blog/src/controller/Controller.php
+++ blog/src/controller/Controller.php
@@ -1,17 +1,30 @@
 <?php
+/**
+*Abstract Class Controller instantiates the twig environement used for generate views
+*
+*/
 
 namespace Blog\Controller;
 
-class Controller
+abstract class Controller
 {
-    protected $twigloader,
-    $twig;
+	/**
+	* Store the templates directories
+	* @var array
+	*/
+    protected $twigloader;
+
+    /**
+    * The twig environement for instantiates twig objects
+    * @var Twig_Environment
+    */
+    protected $twig;
 
     public function __construct()
     {   
     	//load the templates for Twig
         $this->twigloader = new \Twig_Loader_Filesystem(array('src/view', 'src/view/frontend', 'src/view/backend', 'src/view/frontend/form', 'src/view/backend/form'));
-        //load the Twig environment!   
+        //load the Twig environment
         $this->twig = new \Twig_Environment($this->twigloader);
     }
 
--- blog/src/controller/PostController.php
+++ blog/src/controller/PostController.php
@@ -1,20 +1,32 @@
 <?php
+/**
+*Class PostController provides methodes for all related post features
+*
+*/
 
 namespace Blog\Controller;
 
 use Blog\Model\PostManager;
+use Blog\Model\Entity\Post;
 
 class PostController extends Controller
 {   
 
-    //display published article in blog for frontend.
+    /**
+    *display published article in blog for frontend
+    *
+    * @return object Twig
+    * @throws Exception
+    * @throws Twig_Error
+    */
     public function blog()
     {       
             //call manager
            $postManager = new PostManager();
            $blogPosts = $postManager->getPublishedPosts();
 
-            if ($blogPosts->rowCount() === 0) {
+            if ($blogPosts->rowCount() === 0) 
+            {
                 throw new \Exception('Il n\'y a pas encore d\'article publié');
             }
 
@@ -22,10 +34,70 @@ class PostController extends Controller
             echo $this->twig->render('blogView.html.twig', array('blogPosts' => $blogPosts));
     }
 
-    //display form for adding post in backend.
+    /**
+    *display form for adding post in backend
+    *
+    * @return object Twig
+    */
     public function addPostForm()
     {   
         //call view
         echo $this->twig->render('addPostForm.twig');
+
     }
+
+    /**
+    *check and add new post to DB
+    *
+    * @return boolean
+    */
+    public function addPost()
+    {   
+        if (!empty($_POST['postTitle']) && !empty($_POST['postChapo']) && !empty($_POST['postContent']))
+        {
+            $post = new Post($_POST);
+            $postManager = new PostManager();
+
+            $affectedlines = $postManager->addPost($post);
+
+            if ($affectedlines === false) 
+            {
+                throw new \Exception('Impossible d\'ajouter l\'article');
+            }
+
+            else 
+            {
+                header('Location: ../');
+            }
+
+
+        }
+
+        else
+        {
+            throw new \Exception('Merci de remplir la totalité des champs du formulaire');
+        }
+    }
+
+    /**
+    *display view for administrator panel
+    *
+    * @return object Twig
+    */
+    public function postsList()
+    {
+        //call manager
+       $postManager = new PostManager();
+       $postsList = $postManager->getAllPostsList();
+
+        if ($postsList->rowCount() === 0) 
+        {
+            throw new \Exception('Il n\'y a pas encore d\'article publié');
+        }
+
+        //call view
+        echo $this->twig->render('postslist.twig', array('postsList' => $postsList));
+    }
+
+
 }
